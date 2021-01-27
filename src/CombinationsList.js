a535e19c57b34d43c1e2c7a6e86847a5821a8f3a
JAMES ARASIM <jamesarasim@JAMESs-iMac.local>
2021-01-26 21:11:02 -0800

    use select instead of ul

--- src/CombinationsList.js
+++ src/CombinationsList.js
@@ -3,22 +3,19 @@ import * as React from "react";
 
 class CombinationsList extends React.Component {
     render() {
-        console.log("AREA:" + this.props.areaCodeWords);
-        console.log("PREFIX:" + this.props.prefixWords);
-        console.log("SUFFIX:" + this.props.suffixWords);
         let areaCodeList = Array(0);
         let prefixList = Array(0);
         let suffixList = Array(0);
         for(let i=0; i<this.props.areaCodeWords.length; i++){
-            areaCodeList.push(<li>{this.props.areaCodeWords[i]}</li>);
+            areaCodeList.push(<option>{this.props.areaCodeWords[i]}</option>);
         }
         for(let i=0; i<this.props.prefixWords.length; i++){
-            prefixList.push(<li>{this.props.prefixWords[i]}</li>);
+            prefixList.push(<option>{this.props.prefixWords[i]}</option>);
         }
         for(let i=0; i<this.props.suffixWords.length; i++){
-            suffixList.push(<li>{this.props.suffixWords[i]}</li>);
+            suffixList.push(<option>{this.props.suffixWords[i]}</option>);
         }
-        return (<p><ul>{areaCodeList}</ul><ul>{prefixList}</ul><ul>{suffixList}</ul></p>);
+        return (<p><select>{areaCodeList}</select><select>{prefixList}</select><select>{suffixList}</select></p>);
     }
 }
 
