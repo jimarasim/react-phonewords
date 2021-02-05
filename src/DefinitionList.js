ef4551d58439641c88e8a90c372cc1b2b71bfa40
James Arasim <jaemzware@hotmail.com>
2021-02-05 10:35:49 -0800

    implemented  function approach

--- src/DefinitionList.js
+++ src/DefinitionList.js
@@ -1,10 +1,8 @@
 import './index.css';
 import * as React from "react";
 
-class DefinitionList extends React.Component {
-    render() {
-        return (<div className="definitionList"><ul id={this.props.id}></ul></div>);
-    }
+function DefinitionList({id}){
+    return (<div className="definitionList"><ul id={id}></ul></div>);
 }
 
 export default DefinitionList;
