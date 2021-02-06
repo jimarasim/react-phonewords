7fc77d1304b1c4af2448ca3674324b2193ce585d
James Arasim <jaemzware@hotmail.com>
2021-02-06 13:23:02 -0800

    format NumberDisplay

--- src/NumberDisplay.js
+++ src/NumberDisplay.js
@@ -1,12 +1,24 @@
 import './index.css';
-import * as React from "react";
+// import * as React from "react";
 
 function NumberDisplay ({phoneNumber}) {
     return(
-        phoneNumber.map((value, index) =>
-            <>{value} </>
+        phoneNumber.map((value, index) => {
+                return numberFormat(value, index);
+            }
         )
     );
+}
+
+function numberFormat(value, index) {
+    if (index === 0)
+        return "(" + value + " ";
+    else if(index === 2)
+        return value + ") ";
+    else if (index === 5)
+        return value + " - ";
+    else
+        return value + " ";
 }
 
 export default NumberDisplay;
